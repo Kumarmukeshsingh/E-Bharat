331827adaeffaff2732099f923a261ef50ed7df3
MUKESH KUMAR SINGH <89255707+Kumarmukeshsingh@users.noreply.github.com>
2025-09-09 04:31:19 +0000

    hello

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,8 @@ import HeroSection from '../../components/heroSection/HeroSection'
 import Filter from '../../components/filter/Filter'
 import ProductCard from '../../components/productCard/ProductCard'
 import Track from '../../components/track/Track'
-import Testimonial from '../../components/testimonial/Testimonial'
+// import Testimonial from '../../components/testimonial/Testimonial'
+import Testimonial from '../../components/Testimonial/Testimonial';
 import { Link } from 'react-router-dom'
 
 
